3bd254ca53bfad08ad58110cd5a59477697ecab9
Johnny Proano <jproano22@yahoo.com>
2024-09-22 23:26:45 -0400

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Johnny Proano on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
